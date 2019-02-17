b5e9a8a207a0a93c9e806072e5db7268a9f09444
iandelrio <iandelriow@gmail.com>
2019-02-17 08:05:40 -0500

    Removed client directory

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const msRestAzure = require('ms-rest-azure');
+const SQLManagement = require("azure-arm-sql");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile('../views/index.html')
+  res.render('index');
 });
 
 module.exports = router;
