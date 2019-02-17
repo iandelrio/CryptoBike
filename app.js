1902591176d28e9d1784e6f428f4acf02a2c4d80
gov <govi218mu@gmail.com>
2019-02-17 04:17:54 -0500

    change error handler

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/views'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
