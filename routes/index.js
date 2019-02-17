cbc3bacdaa466383693083dccfd79c72a629bf0d
iandelrio <iandelriow@gmail.com>
2019-02-17 02:08:16 -0500

    Desperate measures

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,4 @@ router.get('/', function(req, res, next) {
   res.sendFile('/Users/iandelrio/Documents/Github/CryptoBike/views/index.html')
 });
 
-router.get('/getSharedLayout', function(req, res) {
-	res.sendFile('/Users/iandelrio/Documents/Github/CryptoBike/views/SharedLayout.html')
-})
-
 module.exports = router;
