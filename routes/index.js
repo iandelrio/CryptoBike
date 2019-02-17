1fe56b379bad70b3c44853b628976f5ef8537b94
iandelrio <iandelriow@gmail.com>
2019-02-17 02:43:03 -0500

    Made location relative.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile('/Users/iandelrio/Documents/Github/CryptoBike/views/index.html')
+  res.sendFile('../views/index.html')
 });
 
 module.exports = router;
